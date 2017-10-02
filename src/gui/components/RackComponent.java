5f7a8dd049e853cd2b273402b2e3fb1dab03dbc5
Daniel <daniel_graff_@hotmail.com>
2017-10-02 19:19:55 +0200

    The Rack is almost done. Still the random coloring of the pieces is missing.

--- src/gui/components/RackComponent.java
+++ src/gui/components/RackComponent.java
@@ -5,8 +5,48 @@ import java.awt.*;
 
 public class RackComponent extends JComponent {
 
-    public void paintComponent(Graphics g){
-        g.setColor(Color.white);
-        g.drawString("RACK COMES HERE",300,50);
+    public RackComponent() {
+        this.setVisible(true);
+    }
+
+    int coordinateX;
+    int coordinateY;
+    int radius = 20;
+
+    public void paintComponent(Graphics g)
+    {
+        //g.setColor(Color.white);
+        //g.drawString("RACK COMES HERE",300,50);
+
+        super.paintComponent(g);
+        g.setColor(Color.RED);
+        coordinateX = 25;
+        coordinateY = 25;
+
+        for(int i = 0; i < 6; i++)
+        {
+            Hexagon hexagon = new Hexagon(new Point(coordinateX,coordinateY), radius);
+
+            g.fillPolygon(hexagon.getHexagon());
+
+            coordinateX += 150;
+        }
+        coordinateX = 25;
+        coordinateY = 60;
+
+        for(int i = 0; i < 6; i++)
+        {
+            Hexagon hexagon2 = new Hexagon(new Point(coordinateX,coordinateY), radius);
+
+            g.fillPolygon(hexagon2.getHexagon());
+
+            coordinateX += 150;
+        }
+
+
+
+        //Hexagon hexagon2 = new Hexagon(new Point(40, 20), 10);
+
+
     }
 }
